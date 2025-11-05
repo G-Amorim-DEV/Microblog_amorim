bbbfa6456d74b807b42442129c3d25045a405ee5
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-11-05 15:29:27 -0300

    feat: Adiciona método DAO para atualização completa de usuário
    Implementa a função `atualizar(Usuario $dadosDoUsuario): void` na classe (presumivelmente) de repositório/DAO.
    
    Este método realiza o comando `UPDATE` no banco de dados, permitindo a modificação dos campos:  nome, email, tipo, senha. A atualização é feita através da injeção de uma instância da classe `Usuario`, garantindo a tipagem e o acesso seguro aos dados.
    
    Este método atualiza a senha, independentemente de ela ter sido alterada ou não no formulário de edição (assumindo que o *hash* da senha é passado).

--- src/Services/UsuarioServico.php
+++ src/Services/UsuarioServico.php
@@ -51,8 +51,27 @@ class UsuarioServico{
         se a condição/expressão for válida,
         ela mesam é retornada. Caso contrário, é retornado null */
         return $consulta->fetch() ?: null;
-
     }
 
+    //atualizar (UPDATE/WHERE)
+
+    public function atualizar (Usuario $dadosDoUsuario): void {
+        $sql = "UPDATE usuarios SET
+            nome = :nome,
+            email = :email,
+            tipo = :tipo,
+            senha = :senha
+            WHERE id = :id";
+
+        $consulta = $this->conexao->prepare($sql);
+
+        $consulta->bindValue(':nome', $dadosDoUsuario->getNome());
+        $consulta->bindValue(':email', $dadosDoUsuario->getEmail());
+        $consulta->bindValue(':tipo', $dadosDoUsuario->getTipo());
+        $consulta->bindValue(':senha', $dadosDoUsuario->getSenha());
+        $consulta->bindValue(':id', $dadosDoUsuario->getId());
+
+        $consulta->execute();
+    }
 
 }
